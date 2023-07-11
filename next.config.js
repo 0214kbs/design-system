26e08654a0e2a2faed3aa4f426ac3615311ce07c
0214kbs@naver.com <0214kbs@naver.com>
2023-07-11 16:58:09 +0900

    Chore: project setting 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    reactStrictMode: true,
+    compiler: {
+      styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
+    },
+  };
 module.exports = nextConfig
