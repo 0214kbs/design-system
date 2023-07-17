b07f83f3f094c38582a37de25d4fef73d60afb24
0214kbs@naver.com <0214kbs@naver.com>
2023-07-17 11:49:34 +0900

    chore: svgr 라이브러리 사용 설정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  }, 
     reactStrictMode: true,
     compiler: {
       styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
